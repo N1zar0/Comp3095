e95650fdb89203b185f90dfa57b16bd4740bb7ce
Nizar <atassinizar@gmail.com>
2023-12-09 22:32:14 -0500

    ICE 4

--- microservices-parent/notification-service/src/main/java/ca/gbc/notificationservice/NotificationServiceApplication.java
+++ microservices-parent/notification-service/src/main/java/ca/gbc/notificationservice/NotificationServiceApplication.java
@@ -1,7 +1,10 @@
 package ca.gbc.notificationservice;
 
+import ca.gbc.orderservice.events.OrderPlacedEvent;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
 
 @SpringBootApplication
 @Slf4j
@@ -13,7 +16,7 @@ public class NotificationServiceApplication {
 
     @KafkaListener(topics= "notificationTopic", groupId = "notificationGroup")
     public void handleNotification(OrderPlacedEvent orderPlacedEvent){
-        log.info("sending out email notification for order number: {}")
+        log.info("sending out email notification for order number: {}");
                 orderPlacedEvent.getOrderNumber();
     }
 }
